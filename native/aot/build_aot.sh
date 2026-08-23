b6b4013510893706ea554e9036afdd37c138ec83
szczuru <szczuru@gmail.com>
2026-08-23 20:27:43 +0200

    Copy game.recomp.dll to output directory

--- native/aot/build_aot.sh
+++ native/aot/build_aot.sh
@@ -22,6 +22,8 @@ rm -rf output
 mkdir -p output romfs source
 
 # --- entry: Crash host jeśli leży w managed/, inaczej stary program ---
+    cp -v managed/game.recomp.dll output/
+    cp -v managed/game.recomp.dll output/game/95005139715D6EF9/
 ENTRY_DLL=""
 if [ -f managed/CrashBandicoot.Switch.dll ]; then
     ENTRY_DLL=managed/CrashBandicoot.Switch.dll
